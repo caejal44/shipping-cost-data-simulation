4db5333ed3b878d2ee32707379b1adc23ae5e46c
caejal44 <165009226+caejal44@users.noreply.github.com>
2026-03-12 06:38:47 -0400

    Add "service" to export file, rename columns in export

--- sim/export.py
+++ sim/export.py
@@ -11,10 +11,23 @@ def prepare_for_export(shipment_df: pd.DataFrame, carrier_df: pd.DataFrame) -> p
     shipment_df_copy["delivery_date"] = pd.to_datetime(shipment_df_copy["delivery_date"], errors="coerce")
     shipment_df_copy["delivery_date"] = shipment_df_copy["delivery_date"].dt.strftime("%m/%d/%Y")
 
-    new_order = ["shipment_id", "carrier", "origin_warehouse", "destination_store", "ship_date", "delivery_date", "status",
-                 "weight", "cost", "miles", "transit_days", "lane_id", "carrier_id"]
+    new_order = ["shipment_id", "origin_warehouse", "destination_store", "carrier", "service", "status", "ship_date", "delivery_date",
+                 "weight", "transit_days", "miles", "cost", "lane_id", "carrier_id"]
     shipment_df_copy = shipment_df_copy[new_order]
     shipment_df_copy.drop(columns=["lane_id","carrier_id"], inplace=True)
+    shipment_df_copy.rename(columns={"shipment_id": "Shipment_Id",
+                                     "origin_warehouse": "Origin_Warehouse",
+                                     "destination_store": "Destination",
+                                     "carrier": "Carrier",
+                                     "service": "Service",
+                                     "status": "Status",
+                                     "ship_date": "Shipment_Date",
+                                     "delivery_date": "Delivery_Date",
+                                     "weight": "Weight_kg",
+                                     "transit_days": "Transit_Days",
+                                     "miles": "Distance_Miles",
+                                     "cost": "Cost"})
+
     return shipment_df_copy
 
 def export_shipment_csv(shipment_df):
